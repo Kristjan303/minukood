2a42d1d11616168ee1a69a683811b24d307f809c
Kristjan303 <94617631+Kristjan303@users.noreply.github.com>
2021-11-18 15:12:23 +0200

    ITA21 r2: funktsioonid

---
+++ tunnitöö.py
@@ -0,0 +1,7 @@
+def replacements(sona):
+    sona = sona.replace(" sest ", ", sest ")
+    sona = sona.replace(" aga ", ", aga ")
+    sona = sona.replace(" et ", ", et ")
+    print(sona)
+minulaused = "Ma olen äge sest olen tugev"
+replacements(minulaused)
